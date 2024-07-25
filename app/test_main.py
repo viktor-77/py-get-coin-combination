4eb09c6a69391e32ca4ead692dd84e7d53c7df66
viktor-77 <vumanskiy0664041341@gmail.com>
2024-07-25 14:04:00 +0300

    Resolved Coin combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, result",
+    [
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (25, [0, 0, 0, 1]),
+        (41, [1, 1, 1, 1]),
+    ]
+)
+def test_get_coin_combination(cents: int, result: list) -> None:
+    assert get_coin_combination(cents) == result
